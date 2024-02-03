8612817d99694c80c47c6566e61ece8fdde0be6a
JoeyMucci <jmucci314@gmail.com>
2024-02-02 22:22:36 -0500

    Finished Chapter 6

---
+++ web/src/components/Comment/Comment.stories.jsx
@@ -0,0 +1,30 @@
+// Pass props to your component by passing an `args` object to your story
+//
+// ```jsx
+// export const Primary = {
+//  args: {
+//    propName: propValue
+//  }
+// }
+// ```
+//
+// See https://storybook.js.org/docs/react/writing-stories/args.
+
+import Comment from './Comment'
+
+export const generated = () => {
+  return (
+    <Comment
+      comment={{
+        name: 'Rudolph Wilson',
+        body: 'This is a blog post of superior quality',
+        createdAt: '2024-02-02T12:34:56Z',
+      }}
+    />
+  )
+}
+
+export default {
+  title: 'Components/Comment',
+  component: Comment,
+}
---
+++ web/src/components/CommentsCell/CommentsCell.mock.js
@@ -0,0 +1,16 @@
+export const standard = () => ({
+  comments: [
+    {
+      id: 1,
+      name: 'Cameron Diaz',
+      body: 'Nice work!',
+      createdAt: '2023-01-20T12:34:56Z',
+    },
+    {
+      id: 2,
+      name: 'Price S. Right',
+      body: "Who's still here in 2022?",
+      createdAt: '2022-02-03T23:00:00Z',
+    },
+  ],
+})
